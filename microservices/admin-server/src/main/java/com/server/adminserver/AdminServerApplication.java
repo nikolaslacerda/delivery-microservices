e26be5044ded2792712c7b8f0add1d388037fd03
Nikolas Lacerda <nikolas.lacerda@hotmail.com>
2022-05-24 17:39:08 -0300

    add admin server and improve zipkin tracing

--- microservices/admin-server/src/main/java/com/server/adminserver/AdminServerApplication.java
+++ microservices/admin-server/src/main/java/com/server/adminserver/AdminServerApplication.java
@@ -3,6 +3,7 @@ package com.server.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAdminServer
 @SpringBootApplication
